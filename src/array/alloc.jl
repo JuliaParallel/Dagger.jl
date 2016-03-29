1c26ab9e22f1a255bc56cefe0105320031880f49
Shashi Gowda <shashigowda91@gmail.com>
2016-03-29 23:09:43 +0530

    Add more methods to rand, ones. Introduce randn

--- src/array/alloc.jl
+++ src/array/alloc.jl
@@ -25,6 +25,18 @@ function Base.rand(p::PartitionScheme, eltype::Type, dims)
     AllocateArray(eltype, rand, DenseDomain(map(x->1:x, dims)), p)
 end
 
+Base.rand(p::PartitionScheme, t::Type, dims::Integer...) = rand(p, t, dims)
+Base.rand(p::PartitionScheme, dims::Integer...) = rand(p, Float64, dims)
+Base.rand(p::PartitionScheme, dims::Tuple) = rand(p, Float64, dims)
+
+function Base.randn(p::PartitionScheme, dims)
+    AllocateArray(Float64, (t,sz)->randn(sz), DenseDomain(map(x->1:x, dims)), p)
+end
+Base.randn(p::PartitionScheme, dims::Integer...) = randn(p, dims)
+
 function Base.ones(p::PartitionScheme, eltype::Type, dims)
     AllocateArray(eltype, ones, DenseDomain(map(x->1:x, dims)), p)
 end
+Base.ones(p::PartitionScheme, t::Type, dims::Integer...) = ones(p, t, dims)
+Base.ones(p::PartitionScheme, dims::Integer...) = ones(p, Float64, dims)
+Base.ones(p::PartitionScheme, dims::Tuple) = ones(p, Float64, dims)
