63a595fea196c22133c62278cc83ecac8e9f167c
Shashi Gowda <shashigowda91@gmail.com>
2016-05-02 14:07:54 +0530

    Fixes to gpu tests

--- test/gpu.jl
+++ test/gpu.jl
@@ -1,15 +1,11 @@
 @testset "GPU" begin
 
-@require ArrayFire begin
-
 x = rand(AFArray{Float64}, 5000, 5000)
 y = x * x
 
-xd = Distribute(BlockPartition(4000, 4000), x)
+xd = Distribute(BlockPartition(4000, 4000), Float32, x)
 ctx = [GPUProc(2), GPUProc(3), GPUProc(4)] |> Context
 yd = gather(compute(ctx, xd * xd))
 @test sumabs2(yd - Array(y)) < 1e-10
-
-end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -5,16 +5,25 @@ if VERSION < v"0.5.0-dev"
 else
     using Base.Test
 end
-using Requires
-try
-    Pkg.installed("ArrayFire")
-    using ArrayFire
-    using ComputeFramework
-    include("gpu.jl")
-catch 
-    info("ArrayFire isn't installed. Skipping GPU tests")
-end
-using ComputeFramework
 
-include("domain.jl")
-include("array.jl")
+af_isinstalled = try Pkg.installed("ArrayFire") >= v"0.0.0-" catch er false end
+
+if af_isinstalled
+    try
+        using ArrayFire
+    catch err
+        println(STDERR, "Failed to load ArrayFire. It is not installed properly")
+        rethrow(err)
+    end
+
+    using ComputeFramework
+    include("domain.jl")
+    include("array.jl")
+    include("gpu.jl")
+else
+    using ComputeFramework
+    info("ArrayFire isn't installed. Skipping GPU tests")
+    include("domain.jl")
+    include("array.jl")
+end
+
