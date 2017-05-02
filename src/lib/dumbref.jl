8648182ba5c6cce34922d815ca9ca296f7926e33
Shashi Gowda <shashi@juliacomputing.com>
2017-05-02 19:18:34 +0530

    Another approx_size optimization

--- src/lib/dumbref.jl
+++ src/lib/dumbref.jl
@@ -20,6 +20,14 @@ function approx_size(d)
     Base.summarysize(d) # note: this is accurate but expensive
 end
 
+function approx_size{T}(d::Array{T})
+    if isbits(eltype(T))
+        sizeof(d)
+    else
+        Base.summarysize(d)
+    end
+end
+
 function approx_size(xs::AbstractArray{String})
     # doesn't check for redundant references, but
     # really super fast in comparison to summarysize
