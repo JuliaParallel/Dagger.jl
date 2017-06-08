471c4b223a4c37a24c166c6df2c609cf5f0688b9
Shashi Gowda <shashi@juliacomputing.com>
2017-06-08 16:02:17 +0530

    remove defunct test files

--- test/show.jl
+++
@@ -1,8 +0,0 @@
-a = rand(10,10)
-b = distribute(a)
-c = Dagger.map(x -> x^2, b)
-d = reduce(+, 0.0, c)
-
-@test show(IOBuffer(), b) == 1
-@test show(IOBuffer(), c) == 1
-@test show(IOBuffer(), d) == 1
