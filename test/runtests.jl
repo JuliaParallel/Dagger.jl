e675c380a80ced2f5775af40330cb51b12b54a8c
Julian P Samaroo <jpsamaroo@jpsamaroo.me>
2019-05-25 17:11:14 -0500

    Add fault tolerance to scheduler
    When a worker is killed due to OS signals (such as the Linux OOM
    killer), a ProcessExitedException is likely to be thrown to the head
    node. When such an exception is detected, we reschedule the failed thunk
    on a new, non-dead worker (randomly for now).

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,6 +7,7 @@ using Dagger
 include("domain.jl")
 include("array.jl")
 include("scheduler.jl")
+include("fault-tolerance.jl")
 println(stderr, "tests done. cleaning up...")
 Dagger.cleanup()
 #include("cache.jl")
