59e57bbc5ddec1c962f9b7898145e0c9e614cd5d
Julian P Samaroo <jpsamaroo@jpsamaroo.me>
2019-06-06 11:37:12 -0500

    Clean up debug statements and inline docs

--- src/fault-handler.jl
+++ src/fault-handler.jl
@@ -7,15 +7,6 @@ check_exited_exception(res::ProcessExitedException) = true
 check_exited_exception(res) = false
 
 function handle_fault(ctx, state, thunk, oldproc, chan, node_order)
-    #=
-    @debug "Pre-recovery State:"
-    @show state.ready
-    @show state.running
-    @show keys(state.cache)
-    @show state.waiting
-    @show state.waiting_data
-    =#
-
     # Find thunks whose results were cached on the dead worker
     deadlist = Thunk[thunk]
     thunk.cache = false
@@ -23,7 +14,6 @@ function handle_fault(ctx, state, thunk, oldproc, chan, node_order)
     for t in keys(state.cache)
         v = state.cache[t]
         if v isa Chunk && v.handle isa DRef && v.handle.owner == oldproc.pid
-            @debug "Found dead cached thunk to reschedule: $t"
             push!(deadlist, t)
             # Any inputs to dead cached thunks must be rescheduled
             function bfs!(deadlist, t)
@@ -36,8 +26,7 @@ function handle_fault(ctx, state, thunk, oldproc, chan, node_order)
             bfs!(deadlist, t)
         end
     end
-    #@debug "Deadlist: $deadlist"
-    # TODO: Find thunks who were actively running on the dead worker
+    # TODO: Find *all* thunks who were actively running on the dead worker
 
     # Empty cache of dead thunks
     for ct in keys(state.cache)
@@ -50,30 +39,22 @@ function handle_fault(ctx, state, thunk, oldproc, chan, node_order)
         waiting, waiting_data = state.waiting, state.waiting_data
         off = repeat(" ", offset)
         offi = repeat(" ", offset+1)
-        #@debug "$off Fixing $t"
         if !(t in keys(waiting))
-            #@debug "Not in waiting: $t"
             waiting[t] = Set{Thunk}()
         end
         if !isleaf
-            # If we aren't a leaf thunk, then we may still need to
-            # recover further into the DAG
-            #@debug "$off Begin fix inputs for $t: $(t.inputs)"
+            # If we aren't a leaf thunk, then we may still need to recover
+            # further into the DAG
             for input in t.inputs
-                #@debug "$offi istask input $input of $t: $(istask(input))"
                 istask(input) || continue
-                #@debug "$offi Add input to waiting for $t: $input"
-                #@show state.waiting
-                @assert haskey(waiting, t) "Error: $t not in waiting"
+                @assert haskey(waiting, t) "Error: $t not in state.waiting"
                 push!(waiting[t], input)
                 push!(waiting_data[input], t)
                 isleaf = !(input in deadlist)
                 fix_waitdicts!(state, deadlist, input; isleaf=isleaf, offset=offset+1)
             end
-            #@debug "$off End fix inputs for $t"
         end
         if isempty(waiting[t])
-            #@debug "Prune waiting: $t"
             delete!(waiting, t)
         end
     end
@@ -95,8 +76,6 @@ function handle_fault(ctx, state, thunk, oldproc, chan, node_order)
 
     # Remove dead thunks from state.running, and add state.running
     # deps back to state.waiting
-    # FIXME: If a thunk is actively running on a live node, it may
-    # fail during/after this recovery. Is that a problem?
     wasrunning = copy(state.running)
     empty!(state.running)
     while !isempty(wasrunning)
@@ -116,18 +95,9 @@ function handle_fault(ctx, state, thunk, oldproc, chan, node_order)
             end
             isempty(newtemp) || push!(state.running, newtemp)
         else
-            throw("Unexpected $temp")
+            throw("Unexpected type in recovery: $temp")
         end
     end
-
-    #=
-    @debug "Recovering State:"
-    @show state.ready
-    @show state.running
-    @show keys(state.cache)
-    @show state.waiting
-    @show state.waiting_data
-    =#
 
     # Reschedule inputs from deadlist
     newproc = OSProc(rand(workers()))
@@ -138,7 +108,6 @@ function handle_fault(ctx, state, thunk, oldproc, chan, node_order)
             push!(deadlist, dt)
             continue
         end
-        @debug "Re-scheduling $dt"
         fire_task!(ctx, dt, newproc, state, chan, node_order)
         break
     end
