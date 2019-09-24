1df635c37221eac6eee07685deb165aee2b75375
Julian Samaroo <jpsamaroo@jpsamaroo.me>
2019-09-24 17:47:55 -0500

    Threading trick! (#113)
    * Threading trick!
    
    Spawn `Threads.@spawn f(x...)` instead of `f(x...)` where `x` is the "fetched" data.
    Repeat every process as many times as nthreads() -- causing Dagger to oversubscribe each worker processors as many times as the threads. TOOD: actually get nthreads from each worker rather than using the value on the master process
    Gate threading based on julia version and options
    Add options field to Context and set it in scheduler
    Pass Thunk options down to do_task

--- src/processor.jl
+++ src/processor.jl
@@ -17,13 +17,14 @@ mutable struct Context
     procs::Array{OSProc}
     log_sink::Any
     profile::Bool
+    options
 end
 
 function Context(xs)
-    Context(xs, NoOpLog(), false) # By default don't log events
+    Context(xs, NoOpLog(), false, nothing) # By default don't log events
 end
 Context(xs::Array{Int}) = Context(map(OSProc, xs))
-Context() = Context(workers())
+Context(;nthreads=Threads.nthreads()) = Context([workers() for i=1:nthreads] |> Iterators.flatten |> collect)
 procs(c::Context) = c.procs
 
 """
