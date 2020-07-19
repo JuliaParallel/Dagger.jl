a652352000452ee83d85c3485927f12dba781796
Dilum Aluthge <dilum@aluthge.com>
2020-07-18 22:40:10 -0400

    Fix the deployment of the documentation

--- docs/make.jl
+++ docs/make.jl
@@ -1,10 +1,24 @@
-using Documenter, Dagger
+using Dagger
+using Documenter
 
-makedocs(
-    sitename = "Dagger",
+makedocs(;
+    modules = [Dagger],
+    authors = "JuliaParallel and contributors",
+    repo = "https://github.com/JuliaParallel/Dagger.jl/blob/{commit}{path}#L{line}",
+    sitename = "Dagger.jl",
+    format = Documenter.HTML(;
+        prettyurls = get(ENV, "CI", "false") == "true",
+        canonical = "https://JuliaParallel.github.io/Dagger.jl",
+        assets = String[],
+    ),
     pages = [
         "Home" => "index.md",
         "Processors" => "processors.md",
         "Scheduler Internals" => "scheduler-internals.md",
     ]
 )
+
+deploydocs(;
+    repo="github.com/JuliaParallel/Dagger.jl",
+)
+
