4af08c4170ab65eeb0bfbd0c5436ca01e18df2fc
Julian P Samaroo <jpsamaroo@jpsamaroo.me>
2020-11-04 16:09:39 -0500

    Remove Chunk deserialization

--- src/chunks.jl
+++ src/chunks.jl
@@ -97,20 +97,6 @@ function affinity(r::FileRef)
     end
 end
 
-function Serialization.deserialize(io::AbstractSerializer, dt::Type{Chunk{T,H,P}}) where {T,H,P}
-    nf = fieldcount(dt)
-    c = ccall(:jl_new_struct_uninit, Any, (Any,), dt)
-    Serialization.deserialize_cycle(io, c)
-    for i in 1:nf
-        tag = Int32(read(io.io, UInt8)::UInt8)
-        if tag != Serialization.UNDEFREF_TAG
-            ccall(:jl_set_nth_field, Cvoid, (Any, Csize_t, Any), c, i-1, Serialization.handle_deserialize(io, tag))
-        end
-    end
-    myid() == 1 && nworkers() > 1 && finalizer(x->@async(free!(x)), c)
-    c
-end
-
 """
     tochunk(x, proc; persist=false, cache=false) -> Chunk
 
