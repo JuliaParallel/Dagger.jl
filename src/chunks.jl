df202f1a5182ff9d743142bc5cd19664e3efd107
Julian Samaroo <jpsamaroo@jpsamaroo.me>
2021-07-14 17:57:02 -0500

    Improve Chunk documentation

--- src/chunks.jl
+++ src/chunks.jl
@@ -29,7 +29,9 @@ domain(x::Any) = UnitDomain()
 ###### Chunk ######
 
 """
-A chunk with some data
+A reference to a piece of data located on a remote worker. `Chunk`s are typically created with `Dagger.tochunk(data)`, and the data can then be accessed from any worker with `collect(::Chunk)`. `Chunk`s are serialization-safe, and use distributed refcounting (provided by `MemPool.DRef`) to ensure that the data referenced by a `Chunk` won't be GC'd, as long as a reference exists on some worker.
+
+Each `Chunk` is associated with a given `Dagger.Processor`, which is (in a sense) the processor that "owns" or contains the data. Calling `collect(::Chunk)` will perform data movement and conversions defined by that processor to safely serialize the data to the calling worker.
 """
 mutable struct Chunk{T, H, P<:Processor}
     chunktype::Type{T}
