98dd40e400f4ad2591afa6734e9bbe5f49cc0bb4
Julian P Samaroo <jpsamaroo@jpsamaroo.me>
2021-07-27 10:26:00 -0500

    Add startswith/endswith and nested getproperty to @test_throws_unwrap

--- test/util.jl
+++ test/util.jl
@@ -1,12 +1,38 @@
 macro test_throws_unwrap(terr, args...)
     _test_throws_unwrap(terr, args...)
 end
+function replace_obj!(ex::Expr, obj)
+    if ex.head == :(.)
+        if ex.args[1] isa Symbol
+            ex.args[1] = Expr(:(.), obj, QuoteNode(ex.args[1]))
+        else
+            replace_obj!(ex.args[1], obj)
+        end
+    end
+    return ex
+end
+replace_obj!(ex::Symbol, obj) = Expr(:(.), obj, QuoteNode(ex))
+replace_obj!(ex, obj) = ex
 function _test_throws_unwrap(terr, ex; to_match=[])
     @gensym rerr
     match_expr = Expr(:block)
     for m in to_match
-        @assert m.head == :(=)
-        push!(match_expr.args, :(@test $rerr.$(m.args[1]) == $(m.args[2])))
+        if m.head == :(=)
+            lhs, rhs = replace_obj!(m.args[1], rerr), m.args[2]
+            push!(match_expr.args, :(@test $lhs == $rhs))
+        elseif m.head == :call
+            fn = m.args[1]
+            lhs, rhs = replace_obj!(m.args[2], rerr), m.args[3]
+            if fn == :(<)
+                push!(match_expr.args, :(@test startswith($lhs, $rhs)))
+            elseif fn == :(>)
+                push!(match_expr.args, :(@test endswith($lhs, $rhs)))
+            else
+                push!(match_expr.args, :(@test $fn($lhs, $rhs)))
+            end
+        else
+            error("@test_throws_unwrap cannot handle expr: $m")
+        end
     end
     quote
         $rerr = try
