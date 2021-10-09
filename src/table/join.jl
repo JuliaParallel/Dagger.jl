3d7ca3ff65c06e5cd61dbbd3f6ed4d60402428b2
krynju <gulinski.krystian@gmail.com>
2021-10-10 00:43:54 +0200

    wip first proto, kinda works on namedtuples

--- src/table/join.jl
+++ src/table/join.jl
@@ -1,17 +1,31 @@
-r = (id=10,)
 
-d1=  DTable((id=1:10,a=1.0:10.0), 5)
-d2 = (id=1:10, b=11:20)
 
-function leftjoin(d1::DTable, d2)
-    col = :id
+function leftjoin(d1, d2)
+    sink = Tables.materializer(d1)
+    cols = [:id]
     rowmap = row -> (Tables.getcolumn(row, :id),)
-    match = Vector{Bool}(undef, length(Tables.rows(d1)))
-    for o in Tables.rows(d1)
+    
+    bvec = Vector{Union{Int, Missing}}()
+    schema = Tables.schema(d2)
+    
+    names = filter(x -> x ∉ cols, schema.names)
+    collectors = []
+    for n in names
+        push!(collectors, Vector{Union{Any, Missing}}())
+    end
 
+    for o in Tables.rows(d1)
+        ro = rowmap(o)
+        ri = missing
         for i in Tables.rows(d2)
-        
-            continue
+            if ro == rowmap(i)
+                ri = i
+                break
+            end
+        end
+        for (idx,n) in enumerate(names)
+            push!(collectors[idx], ri === missing ? missing : Tables.getcolumn(ri, n))
         end
     end
+    sink((;Tables.columntable(d1)..., [n => collectors[idx] for (idx, n) in enumerate(names)]...))
 end
