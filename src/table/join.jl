adc26677b95e9a8f0a69988faa00c2a22f3dfbcd
krynju <gulinski.krystian@gmail.com>
2021-10-10 18:04:18 +0200

    working join proto

--- src/table/join.jl
+++ src/table/join.jl
@@ -1,31 +1,74 @@
+function _resolve_indices(d1, d2, on)
+    isnothing(on) && error("yeet")
+    if on isa Symbol
+        l_cols = [on]
+        r_cols = [on]
+    elseif on isa Vector{Symbol}
+        l_cols = on
+        r_cols = on
+    elseif on isa Vector{Pair{Symbol, Symbol}}
+        l_cols = getindex.(on, 1)
+        r_cols = getindex.(on, 2)
+    end
+    d1_names = collect(Tables.schema(d1).names)
+    d2_names = collect(Tables.schema(d2).names)
+    _l = length(l_cols)
+    l_cols_indices = NTuple{_l, Int}(indexin(l_cols, d1_names))
+    r_cols_indices = NTuple{_l, Int}(indexin(r_cols, d2_names))
+    l_cols_indices, r_cols_indices
+end
+
+function leftjoin(d1::DTable, d2; on=nothing, d2_mode=:full)
+    l_cols_indices, r_cols_indices = _resolve_indices(d1, d2, on)
+    _a = setdiff(1:length(Tables.columnnames(d2)), r_cols_indices)
+    r_new_ind = NTuple{length(_a), Int}(_a)
+    v = [Dagger.@spawn _leftjoin(c, d2, l_cols_indices, r_cols_indices,r_new_ind) for c in d1.chunks]
+    DTable(v, d1.tabletype)
+end
 
 
-function leftjoin(d1, d2)
-    sink = Tables.materializer(d1)
-    cols = [:id]
-    rowmap = row -> (Tables.getcolumn(row, :id),)
-    
-    bvec = Vector{Union{Int, Missing}}()
-    schema = Tables.schema(d2)
-    
-    names = filter(x -> x ∉ cols, schema.names)
-    collectors = []
-    for n in names
-        push!(collectors, Vector{Union{Any, Missing}}())
+
+# per partition, full r table joining
+function _leftjoin(l, r, l_ind::NTuple{N1,Int}, r_ind::NTuple{N1,Int}, r_new_ind::NTuple{N2,Int}) where {N1, N2}
+    sink = Tables.materializer(l)
+    collectors = Vector{Vector}()
+    sizehint!(collectors, length(r_new_ind))
+
+    rschema = Tables.schema(r)
+    rnames = rschema.names
+    llength = length(Tables.rows(l))
+
+    @inbounds for i in r_new_ind
+        t = isnothing(rschema.types) ? Any : rschema.types[i]
+        p = Vector{Union{t, Missing}}()
+        sizehint!(p, llength)
+        push!(collectors, p)
     end
 
-    for o in Tables.rows(d1)
-        ro = rowmap(o)
-        ri = missing
-        for i in Tables.rows(d2)
-            if ro == rowmap(i)
-                ri = i
+    @inbounds for o in Tables.rows(l)
+        for i in Tables.rows(r)
+            if compare_rows(o, i, l_ind, r_ind)
+                for j in 1:length(r_new_ind)
+                    push!(collectors[j], Tables.getcolumn(i, r_new_ind[j]))
+                end
                 break
             end
         end
-        for (idx,n) in enumerate(names)
-            push!(collectors[idx], ri === missing ? missing : Tables.getcolumn(ri, n))
+        @inbounds for j in 1:N2
+            push!(collectors[j], missing)
         end
     end
-    sink((;Tables.columntable(d1)..., [n => collectors[idx] for (idx, n) in enumerate(names)]...))
+
+    left_cols = Tables.columntable(l)
+    new_cols = [rnames[col_idx] => collectors[i] for (i, col_idx) in enumerate(r_new_ind)]
+    sink((;left_cols..., new_cols...))
+end
+
+function compare_rows(o, i, l_ind::NTuple{N, Int}, r_ind::NTuple{N, Int}) where {N}
+    test = true
+    @inbounds for x in 1:N
+        test &= Tables.getcolumn(o, l_ind[x]) == Tables.getcolumn(i, r_ind[x])
+        test || break
+    end
+    test
 end
