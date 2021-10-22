98fc01a563c0faabbca01846c99c08a63f5fd69e
Julian P Samaroo <jpsamaroo@jpsamaroo.me>
2021-10-22 12:15:58 -0500

    Document TableStorage usage

--- src/lib/logging-extras.jl
+++ src/lib/logging-extras.jl
@@ -216,7 +216,30 @@ deletion_hook(x, idx) = nothing
 """
     TableStorage
 
-LogWindow-compatible aggregator which stores logs in a Tables.jl-compatible sink.
+LogWindow-compatible aggregator which stores logs in a Tables.jl-compatible
+sink.
+
+Using a `TableStorage` is reasonably simple:
+
+```julia
+ml = Dagger.MultiEventLog()
+
+... # Add some events
+
+lw = Dagger.Events.LogWindow(5*10^9, :core)
+
+# Create a DataFrame with one Any[] for each event
+df = DataFrame([key=>[] for key in keys(ml.consumers)]...)
+
+# Create the TableStorage and register its creation handler
+ts = Dagger.Events.TableStorage(df)
+push!(lw.creation_handlers, ts)
+
+ml.aggregators[:lw] = lw
+
+# Logs will now be saved into `df` automatically, and packages like
+# DaggerWebDash.jl will automatically use it to retrieve subsets of the logs.
+```
 """
 struct TableStorage{T}
     sink::T
