dc56d749dda390b478d982c231e476a8022c4c07
krynju <gulinski.krystian@gmail.com>
2021-11-12 23:36:12 +0100

    redo the interface to enable custom kwargs

--- src/table/join_interface.jl
+++ src/table/join_interface.jl
@@ -1,29 +1,52 @@
 import DataAPI: leftjoin, innerjoin
 
+const JOINKWARGS = Set([
+    :l_sorted,
+    :r_sorted,
+    :r_unique,
+    :build_lookup,
+    :lookup_table,])
 
-function leftjoin(d1::DTable, d2; on = nothing)
-    f = (_d1, _d2, _on) -> leftjoin(_d1, _d2, on = _on)
-    v = [Dagger.@spawn f(c, d2, on) for c in d1.chunks]
+
+function leftjoin(d1::DTable, d2; kwargs...)
+    f = (_d1, _d2, kwargs) -> leftjoin(_d1, _d2; kwargs...)
+    if any(k in JOINKWARGS for k in keys(kwargs))
+        f = (_d1, _d2, kwargs) -> _leftjoin(_d1, _d2; kwargs...)
+    end
+
+    v = [Dagger.@spawn f(c, d2, kwargs) for c in d1.chunks]
     DTable(v, d1.tabletype)
 end
 
-function leftjoin(l, r; on = nothing)
-    names, _, other_r, cmp_l, cmp_r = resolve_colnames(l, r, on)
-    inner_l, inner_r = match_inner_indices(l, r, cmp_l, cmp_r)
-    outer_l = find_outer_indices(l, inner_l)
-    build_joined_table(:leftjoin, names, l, r, inner_l, inner_r, outer_l, other_r)
-end
+leftjoin(l, r; on = nothing) = _leftjoin(l, r; on = on)
+_leftjoin(l, r; kwargs...) = _join(:leftjoin, l, r; kwargs...)
 
 
-function innerjoin(d1::DTable, d2; on = nothing)
-    f = (_d1, _d2, _on) -> innerjoin(_d1, _d2, on = _on)
-    v = [Dagger.@spawn f(c, d2, on) for c in d1.chunks]
+function innerjoin(d1::DTable, d2; kwargs...)
+    f = (_d1, _d2, kwargs) -> innerjoin(_d1, _d2; kwargs...)
+    if any(k in JOINKWARGS for k in keys(kwargs))
+        f = (_d1, _d2, kwargs) -> _innerjoin(_d1, _d2; kwargs...)
+    end
+    v = [Dagger.@spawn f(c, d2, kwargs) for c in d1.chunks]
     DTable(v, d1.tabletype)
 end
 
-function innerjoin(l, r; on = nothing)
+innerjoin(l, r; on = nothing) = _innerjoin(l, r; on = on)
+_innerjoin(l, r; kwargs...) = _join(:innerjoin, l, r; kwargs...)
+
+
+function _join(
+        type::Symbol,
+        l, r;
+        on = nothing,
+        l_sorted = false,
+        r_sorted = false,
+        r_unique = false,
+        build_lookup = false,
+        lookup_table = nothing
+    )
     names, _, other_r, cmp_l, cmp_r = resolve_colnames(l, r, on)
     inner_l, inner_r = match_inner_indices(l, r, cmp_l, cmp_r)
-    outer_l = Set{UInt}()
-    build_joined_table(:innerjoin, names, l, r, inner_l, inner_r, outer_l, other_r)
+    outer_l = type == :innerjoin ? Set{UInt}() : find_outer_indices(l, inner_l)
+    build_joined_table(type, names, l, r, inner_l, inner_r, outer_l, other_r)
 end
