f65eee665cec900f5795b60f5117bbbd3f3d2195
krynju <gulinski.krystian@gmail.com>
2022-04-05 23:09:18 +0200

    add poc

--- src/table/scratch.jl
+++ src/table/scratch.jl
@@ -12,35 +12,44 @@ d1 = DTable([Dagger.@spawn genchunk(MersenneTwister(1111 + i)) for i in 1:nchunk
 
 d2 = fetch(d1, DataFrame)
 
-select(d2, :a1 => sum)
-precalc_column_val = sum(Tables.getcolumn(d1, :a1)) # @spawn probably
-dt = map(r -> (a1_sum=precalc_column_val,), d1)
+select(d2,
+    :a1,
+    :a1 => sum,
+    AsTable([:a1, :a2]) => ByRow(sum)
+)
+
+precalc_column_val = Dagger.@spawn ((d) -> sum(Tables.getcolumn(d, :a1)))(d1)
+val = fetch(precalc_column_val)
+
+dt = map(r -> (
+        a1=r.a1,
+        a1_sum=val,
+        a1_a2_sum=sum(Tables.getcolumn.(Ref(r), [:a1, :a2]))
+    ), d1)
 fetch(dt, DataFrame)
 
 
-select(d2, AsTable([:a1, :a2]) => ByRow(sum))
-dt = map(r -> (a1_a2_sum=sum(Tables.getcolumn.(Ref(r), [:a1, :a2])),), d1)
+# for combine length checks are needed and more logic
+
+combine(d2, :a1 => ByRow(sin) => :c,)
+dt = map(r -> (c=sin(Tables.getcolumn(r, :a1)),), d1)
 fetch(dt, DataFrame)
-
-
-
-
-
-combine(d2, :a1 => ByRow(sin) => :c, :a2)
-dt = map(r -> (c = sin(Tables.getcolumn(r, :a1)), a2 = Tables.getcolumn(r, :a2),), d1)
-fetch(dt, DataFrame)
-
 
 combine(d2, :a1 => sum => :c, :a2)
-precalc_column_val = sum(Tables.getcolumn(d1, :a1)) # @spawn probably
-# length check?
-dt = map(r -> (c = precalc_column_val, a2 = Tables.getcolumn(r, :a2)), d1)
+precalc_column_val = Dagger.@spawn ((d) -> sum(Tables.getcolumn(d, :a1)))(d1) # @spawn probably
+val = fetch(precalc_column_val)
+dt = map(r -> (c=val, a2=r.a2), d1)
 fetch(dt, DataFrame)
-
 
 combine(d2, :a1 => sum => :c)
-precalc_column_val = sum(Tables.getcolumn(d1, :a1)) # @spawn probably
+precalc_column_val = Dagger.@spawn ((d) -> sum(Tables.getcolumn(d1, :a1)))(d1) # @spawn probably
 # partitioning here is a problem, we don't know the size of the result and all columns of a row need to be in the same chunk
-dt = DTable((c = [precalc_column_val...], ), 1000)
+dt = DTable((c=[fetch(precalc_column_val)...],), 1000)
 fetch(dt, DataFrame)
 
+
+# We have ByRow/AsTable - maybe we could have Reduce to support reduction functions?
+# combine(d, :a => Reduce(+))
+# dt = DTable((a_reduce = [fetch(reduce(+, d, cols=:a))...],), 100)
+# or 
+# using OnlineStats; combine(d, :a => Reduce(fit!, init=Mean()))
