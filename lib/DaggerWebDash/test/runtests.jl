f6b19f6651b4d6bf44ec908e30ff1a9627f9ebd3
Julian P Samaroo <jpsamaroo@jpsamaroo.me>
2022-07-30 11:57:37 -0500

    CI: Add DaggerWebDash test job

--- lib/DaggerWebDash/test/runtests.jl
+++ lib/DaggerWebDash/test/runtests.jl
@@ -1,4 +1,5 @@
 using DaggerWebDash
+import DaggerWebDash: LinePlot, GanttPlot
 using TimespanLogging
 using Dagger
 using Test
@@ -18,22 +19,20 @@ using Test
     # FIXME: logs_df = DataFrame([key=>[] for key in keys(ml.consumers)]...)
     # ts = DaggerWebDash.TableStorage(logs_df)
     # push!(lw.creation_handlers, ts)
-    if render == "webdash"
-        d3r = DaggerWebDash.D3Renderer(8080) #; seek_store=ts)
-        push!(lw.creation_handlers, d3r)
-        push!(lw.deletion_handlers, d3r)
-        push!(d3r, GanttPlot(:core, :id, :timeline, :esat, :psat, "Overview"))
-        # TODO: push!(d3r, ProfileViewer(:core, :profile, "Profile Viewer"))
-        push!(d3r, LinePlot(:core, :wsat, "Worker Saturation", "Running Tasks"))
-        push!(d3r, LinePlot(:core, :loadavg, "CPU Load Average", "Average Running Threads"))
-        push!(d3r, LinePlot(:core, :bytes, "Allocated Bytes", "Bytes"))
-        push!(d3r, LinePlot(:core, :mem, "Available Memory", "% Free"))
-        #push!(d3r, GraphPlot(:core, :id, :timeline, :profile, "DAG"))
-        ml.aggregators[:d3r] = d3r
-    end
+    d3r = DaggerWebDash.D3Renderer(8080) #; seek_store=ts)
+    push!(lw.creation_handlers, d3r)
+    push!(lw.deletion_handlers, d3r)
+    push!(d3r, GanttPlot(:core, :id, :timeline, :esat, :psat, "Overview"))
+    # TODO: push!(d3r, ProfileViewer(:core, :profile, "Profile Viewer"))
+    push!(d3r, LinePlot(:core, :wsat, "Worker Saturation", "Running Tasks"))
+    push!(d3r, LinePlot(:core, :loadavg, "CPU Load Average", "Average Running Threads"))
+    push!(d3r, LinePlot(:core, :bytes, "Allocated Bytes", "Bytes"))
+    push!(d3r, LinePlot(:core, :mem, "Available Memory", "% Free"))
+    #push!(d3r, GraphPlot(:core, :id, :timeline, :profile, "DAG"))
+    ml.aggregators[:d3r] = d3r
     ml.aggregators[:logwindow] = lw
     ctx = Context(; log_sink=ml)
-    A = rand(Blocks(4, 4), 16, 16)
-    @async collect(A * A)
+    Dagger.Sch.EAGER_CONTEXT[] = ctx
+    fetch(Dagger.@spawn 1+1)
     run(pipeline(`curl -s localhost:8080/index.html`; stdout=devnull))
 end
