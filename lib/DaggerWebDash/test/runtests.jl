a81a94b013f1b4f0078f1cf9e2daa806dc1156c0
Julian P Samaroo <jpsamaroo@jpsamaroo.me>
2022-07-30 11:57:23 -0500

    DaggerWebDash: Add basic test

---
+++ lib/DaggerWebDash/test/runtests.jl
@@ -0,0 +1,39 @@
+using DaggerWebDash
+using TimespanLogging
+using Dagger
+using Test
+
+@testset "Basics" begin
+    ml = TimespanLogging.MultiEventLog()
+    ml[:core] = TimespanLogging.Events.CoreMetrics()
+    ml[:id] = TimespanLogging.Events.IDMetrics()
+    #profile && (ml[:profile] = DaggerWebDash.ProfileMetrics())
+    ml[:wsat] = TimespanLogging.Events.WorkerSaturation()
+    ml[:loadavg] = TimespanLogging.Events.CPULoadAverages()
+    ml[:bytes] = Dagger.Events.BytesAllocd()
+    ml[:mem] = TimespanLogging.Events.MemoryFree()
+    ml[:esat] = TimespanLogging.Events.EventSaturation()
+    ml[:psat] = Dagger.Events.ProcessorSaturation()
+    lw = TimespanLogging.Events.LogWindow(5*10^9, :core)
+    # FIXME: logs_df = DataFrame([key=>[] for key in keys(ml.consumers)]...)
+    # ts = DaggerWebDash.TableStorage(logs_df)
+    # push!(lw.creation_handlers, ts)
+    if render == "webdash"
+        d3r = DaggerWebDash.D3Renderer(8080) #; seek_store=ts)
+        push!(lw.creation_handlers, d3r)
+        push!(lw.deletion_handlers, d3r)
+        push!(d3r, GanttPlot(:core, :id, :timeline, :esat, :psat, "Overview"))
+        # TODO: push!(d3r, ProfileViewer(:core, :profile, "Profile Viewer"))
+        push!(d3r, LinePlot(:core, :wsat, "Worker Saturation", "Running Tasks"))
+        push!(d3r, LinePlot(:core, :loadavg, "CPU Load Average", "Average Running Threads"))
+        push!(d3r, LinePlot(:core, :bytes, "Allocated Bytes", "Bytes"))
+        push!(d3r, LinePlot(:core, :mem, "Available Memory", "% Free"))
+        #push!(d3r, GraphPlot(:core, :id, :timeline, :profile, "DAG"))
+        ml.aggregators[:d3r] = d3r
+    end
+    ml.aggregators[:logwindow] = lw
+    ctx = Context(; log_sink=ml)
+    A = rand(Blocks(4, 4), 16, 16)
+    @async collect(A * A)
+    run(pipeline(`curl -s localhost:8080/index.html`; stdout=devnull))
+end
