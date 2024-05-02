a6710ab093087878a8b3f66bb9a63bbabf28e7e9
Julian P Samaroo <jpsamaroo@jpsamaroo.me>
2024-05-02 12:48:48 -0700

    streaming: Fix concurrency issues

--- src/stream-fetchers.jl
+++ src/stream-fetchers.jl
@@ -1,25 +1,31 @@
 struct RemoteFetcher end
 function stream_fetch_values!(::Type{RemoteFetcher}, T, store_ref::Chunk{Store_remote}, buffer::Blocal, id::UInt) where {Store_remote, Blocal}
-    if store_ref.handle.owner == myid()
-        store = fetch(store_ref)::Store_remote
-        while !isfull(buffer)
+    thunk_id = STREAM_THUNK_ID[]
+    @dagdebug thunk_id :stream "fetching values"
+    @label fetch_values
+    # FIXME: Pass buffer free space
+    # TODO: It would be ideal if we could wait on store.lock, but get unlocked during migration
+    values = MemPool.access_ref(store_ref.handle, id, T, Store_remote, thunk_id) do store, id, T, Store_remote, thunk_id
+        if !isopen(store)
+            throw(InvalidStateException("Stream is closed", :closed))
+        end
+        @dagdebug thunk_id :stream "trying to fetch values at $(myid())"
+        store::Store_remote
+        in_store = store
+        STREAM_THUNK_ID[] = thunk_id
+        values = T[]
+        while !isempty(store, id)
             value = take!(store, id)::T
-            put!(buffer, value)
+            push!(values, value)
         end
-    else
-        thunk_id = STREAM_THUNK_ID[]
-        values = remotecall_fetch(store_ref.handle.owner, store_ref.handle, id, T, Store_remote) do store_ref, id, T, Store_remote
-            STREAM_THUNK_ID[] = thunk_id
-            store = MemPool.poolget(store_ref)::Store_remote
-            values = T[]
-            while !isempty(store, id)
-                value = take!(store, id)::T
-                push!(values, value)
-            end
-            return values
-        end::Vector{T}
-        for value in values
-            put!(buffer, value)
-        end
+        return values
+    end::Vector{T}
+    if isempty(values)
+        @goto fetch_values
+    end
+
+    @dagdebug thunk_id :stream "fetched $(length(values)) values"
+    for value in values
+        put!(buffer, value)
     end
 end
