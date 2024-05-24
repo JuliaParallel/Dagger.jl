b0674d925c2252a9c507efb40d2387ed5a349e2c
JamesWrigley <james@puiterwijk.org>
2024-05-24 18:29:48 +0200

    Add a --verbose option to runtests.jl
    This is a bit nicer than commenting/uncommenting a line in the code.

--- test/runtests.jl
+++ test/runtests.jl
@@ -51,6 +51,9 @@ if PROGRAM_FILE != "" && realpath(PROGRAM_FILE) == @__FILE__
                 arg_type = Int
                 default = additional_workers
                 help = "How many additional workers to launch"
+            "-v", "--verbose"
+                action = :store_true
+                help = "Run the tests with debug logs from Dagger"
         end
     end
 
@@ -80,6 +83,10 @@ if PROGRAM_FILE != "" && realpath(PROGRAM_FILE) == @__FILE__
     parsed_args["simulate"] && exit(0)
 
     additional_workers = parsed_args["procs"]
+
+    if parsed_args["verbose"]
+        ENV["JULIA_DEBUG"] = "Dagger"
+    end
 else
     to_test = all_test_names
     @info "Running all tests"
