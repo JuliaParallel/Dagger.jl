65ab368d8468c9b4f46af4e9ab123e5a29ab9aa0
pszufe <pszufe@gmail.com>
2024-07-12 17:02:26 +0200

    fixup! fixup! fixup! Add rand! implementation

--- ext/DistributionsExt.jl
+++ ext/DistributionsExt.jl
@@ -9,7 +9,7 @@ end
 using Dagger, Random
 
 Random.rand!(s::Sampleable, dm::DArray{T}) where T = map!(() -> rand(s), dm)
-Random.rand!(s::Sampleable{Univariate}, dm::DArray{T}) where T = map!(() -> rand(s), dm)
-Random.rand!(s::Sampleable{ArrayLikeVariate{M}}, dm::DArray{T}) where {M, T} = map!(() -> rand(s), dm)
+Random.rand!(rng::AbstractRNG, s::Sampleable{Univariate}, dm::DArray{T}) where T = map!(() -> rand(rng, s), dm)
+Random.rand!(rng::AbstractRNG, s::Sampleable{ArrayLikeVariate{M}}, dm::DArray{T}) where {M, T} = map!(() -> rand(rng, s), dm)
 
 end # module DistributionsExt
