4b17ce8571f407513fb334de8f05027abf0f65bf
Julian P Samaroo <jpsamaroo@jpsamaroo.me>
2024-07-21 14:22:30 -0500

    fixup! fixup! fixup! fixup! Add rand! implementation

--- src/array/random.jl
+++ src/array/random.jl
@@ -1,6 +1,6 @@
 using Random
 
-Random.rand!(dm::DArray{T}) where T = map!(() -> rand(T), dm)
-Random.rand!(rng::AbstractRNG, dm::DArray{T}) where T = map!(() -> rand(rng), dm)
-Random.randn!(dm::DArray{T}) where T = map!(() -> randn(), dm)
-Random.randn!(rng::AbstractRNG, dm::DArray{T}) where T = map!(() -> randn(rng), dm)
+Random.rand!(dm::DArray{T}) where T = map!(_ -> rand(T), dm)
+Random.rand!(rng::AbstractRNG, dm::DArray{T}) where T = map!(_ -> rand(rng, T), dm)
+Random.randn!(dm::DArray{T}) where T = map!(_ -> randn(T), dm)
+Random.randn!(rng::AbstractRNG, dm::DArray{T}) where T = map!(_ -> randn(rng, T), dm)
