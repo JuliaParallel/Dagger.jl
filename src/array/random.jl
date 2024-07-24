65ffd4e90a6be731f0aa38fe97c1a0511f348f86
Julian P Samaroo <jpsamaroo@jpsamaroo.me>
2024-07-24 16:14:38 -0500

    fixup! fixup! fixup! fixup! fixup! Add rand! implementation

--- src/array/random.jl
+++ src/array/random.jl
@@ -1,6 +1,50 @@
 using Random
 
-Random.rand!(dm::DArray{T}) where T = map!(_ -> rand(T), dm)
-Random.rand!(rng::AbstractRNG, dm::DArray{T}) where T = map!(_ -> rand(rng, T), dm)
-Random.randn!(dm::DArray{T}) where T = map!(_ -> randn(T), dm)
-Random.randn!(rng::AbstractRNG, dm::DArray{T}) where T = map!(_ -> randn(rng, T), dm)
+# Hide Future in a module to not mess with Distributed.Future
+module RandJump
+    using Random
+    using Future
+
+    function randjump(rng::AbstractRNG, n::Integer)
+        new_rng = copy(rng)
+        for _ in 1:n
+            rand(new_rng)
+        end
+        return new_rng
+    end
+    randjump(rng::MersenneTwister, n::Integer) = Future.randjump(rng, n)
+end # module RandJump
+
+function Random.rand!(dm::DArray{T}) where T
+    # Advance the RNG to satisy the rand API
+    rand()
+    return map!(_ -> rand(T), dm)
+end
+function Random.randn!(dm::DArray{T}) where T
+    # Advance the RNG to satisy the rand API
+    randn()
+    return map!(_ -> randn(T), dm)
+end
+
+function Random.rand!(rng::AbstractRNG, A::DArray{T}) where T
+    part_sz = prod(map(length, first(A.subdomains).indexes))
+    Dagger.spawn_datadeps() do
+        for Ac in chunks(A)
+            rng = RandJump.randjump(rng, part_sz)
+            new_rng = copy(rng)
+            Dagger.@spawn map!(_->rand(new_rng, T), InOut(Ac), Ac)
+        end
+    end
+    return A
+end
+function Random.randn!(rng::AbstractRNG, A::DArray{T}) where T
+    part_sz = prod(map(length, first(A.subdomains).indexes))
+    Dagger.spawn_datadeps() do
+        for Ac in chunks(A)
+            rng = RandJump.randjump(rng, part_sz)
+            new_rng = copy(rng)
+            Dagger.@spawn map!(_->randn(new_rng, T), InOut(Ac), Ac)
+        end
+    end
+    return A
+end
