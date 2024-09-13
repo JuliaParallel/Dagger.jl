79ee021e16aec745325f3949550dba6d3445596c
Julian P Samaroo <jpsamaroo@jpsamaroo.me>
2024-09-13 12:18:56 -0400

    fixup! task-tls: Refactor into DTaskTLS struct

--- src/task-tls.jl
+++ src/task-tls.jl
@@ -10,22 +10,6 @@ end
 const DTASK_TLS = TaskLocalValue{Union{DTaskTLS,Nothing}}(()->nothing)
 
 """
-    in_task() -> Bool
-
-Returns `true` if currently executing in a [`DTask`](@ref), else `false`.
-"""
-in_task() = DTASK_TLS[] !== nothing
-@deprecate in_thunk() in_task()
-
-"""
-    task_processor() -> Processor
-
-Get the current processor executing the current [`DTask`](@ref).
-"""
-task_processor() = get_tls().processor
-@deprecate thunk_processor() task_processor()
-
-"""
     get_tls() -> DTaskTLS
 
 Gets all Dagger TLS variable as a `DTaskTLS`.
@@ -40,3 +24,19 @@ Sets all Dagger TLS variables from `tls`, which may be a `DTaskTLS` or a `NamedT
 function set_tls!(tls)
     DTASK_TLS[] = DTaskTLS(tls.processor, tls.sch_uid, tls.sch_handle, tls.task_spec)
 end
+
+"""
+    in_task() -> Bool
+
+Returns `true` if currently executing in a [`DTask`](@ref), else `false`.
+"""
+in_task() = DTASK_TLS[] !== nothing
+@deprecate in_thunk() in_task()
+
+"""
+    task_processor() -> Processor
+
+Get the current processor executing the current [`DTask`](@ref).
+"""
+task_processor() = get_tls().processor
+@deprecate thunk_processor() task_processor()
