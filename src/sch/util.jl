5c20e9129565e5dbaba020f2575a4b5dfc5163d0
Julian P Samaroo <jpsamaroo@jpsamaroo.me>
2024-09-14 11:54:34 -0400

    Sch: Add unwrap_nested_exception for DTaskFailedException

--- src/sch/util.jl
+++ src/sch/util.jl
@@ -29,6 +29,8 @@ unwrap_nested_exception(err::CapturedException) =
     unwrap_nested_exception(err.ex)
 unwrap_nested_exception(err::RemoteException) =
     unwrap_nested_exception(err.captured)
+unwrap_nested_exception(err::DTaskFailedException) =
+    unwrap_nested_exception(err.ex)
 unwrap_nested_exception(err) = err
 
 "Gets a `NamedTuple` of options propagated by `thunk`."
