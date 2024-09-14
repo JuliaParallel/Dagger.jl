c38f44a273b70194f056499fd000c4ec4f69c81c
Julian P Samaroo <jpsamaroo@jpsamaroo.me>
2024-09-14 12:03:34 -0400

    streaming: Buffers and tasks per input/output
    Instead of taking/putting values sequentially (which may block), runs
    "pull" and "push" tasks for each input and output, respectively. Uses
    buffers to communicate values between pullers/pushers and the streaming
    task, instead of only using one buffer per task-to-task connection.

--- src/stream-transfer.jl
+++ src/stream-transfer.jl
@@ -1,13 +1,14 @@
 struct RemoteFetcher end
-function stream_fetch_values!(::Type{RemoteFetcher}, T, store_ref::Chunk{Store_remote}, buffer::Blocal, id::UInt) where {Store_remote, Blocal}
+function stream_pull_values!(::Type{RemoteFetcher}, T, store_ref::Chunk{Store_remote}, buffer::Blocal, id::UInt) where {Store_remote, Blocal}
     thunk_id = STREAM_THUNK_ID[]
     @dagdebug thunk_id :stream "fetching values"
 
     values = T[]
+    free_space = length(buffer.buffer) - length(buffer)
     while isempty(values)
         # FIXME: Pass buffer free space
         # TODO: It would be ideal if we could wait on store.lock, but get unlocked during migration
-        values = MemPool.access_ref(store_ref.handle, id, T, Store_remote, thunk_id) do store, id, T, Store_remote, thunk_id
+        values = MemPool.access_ref(store_ref.handle, id, T, Store_remote, thunk_id, free_space) do store, id, T, Store_remote, thunk_id, free_space
             if !isopen(store)
                 throw(InvalidStateException("Stream is closed", :closed))
             end
@@ -16,8 +17,9 @@ function stream_fetch_values!(::Type{RemoteFetcher}, T, store_ref::Chunk{Store_r
             in_store = store
             STREAM_THUNK_ID[] = thunk_id
             values = T[]
-            while !isempty(store, id)
+            while !isempty(store, id) && length(values) < free_space
                 value = take!(store, id)::T
+                @dagdebug thunk_id :stream "fetched $value"
                 push!(values, value)
             end
             return values
@@ -35,4 +37,7 @@ function stream_fetch_values!(::Type{RemoteFetcher}, T, store_ref::Chunk{Store_r
     for value in values
         put!(buffer, value)
     end
+end
+function stream_push_values!(::Type{RemoteFetcher}, T, store_ref::Store_remote, buffer::Blocal, id::UInt) where {Store_remote, Blocal}
+    sleep(0.1)
 end
