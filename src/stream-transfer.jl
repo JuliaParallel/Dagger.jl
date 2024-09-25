1998cbb76667603ad8863a726904aa01610b9d2f
davidizzle <davide.ferretti.j@gmail.com>
2024-09-25 23:32:04 +0200

    bulk of the functions

--- src/stream-transfer.jl
+++ src/stream-transfer.jl
@@ -101,6 +101,213 @@ struct ZeroMQ
 end
 =#
 
+# UDP dispatch
+function stream_pull_values!(udp::UDP, T, our_store::StreamStore, their_stream::Stream, buffer)
+    udpsock = UDPSocket
+    bind(udpsock, udp.protocol.ip, udp.protocol.port)
+
+    values = T[]
+    values = recvfrom(udpsock)
+    data = reinterpret(T, data)
+
+    for value in values
+        put!(buffer, value)
+    end
+end
+function stream_push_values!(udp::UDP, T, our_store::StreamStore, their_stream::Stream, buffer)
+    values = _load_val_from_buffer!(buffer, T)
+    udpsock = UDPSocket()
+    send(udpsock, udp.protocol.ip, udp.protocol.port, values)
+    close(udpsock)
+end
+
+# TCP dispatch
+function stream_pull_values!(tcp::TCP, T, our_store::StreamStore, their_stream::Stream, buffer)
+    @label pull_values_TCP
+    try
+        server = listen(tcp.protocol.ip, tcp.protocol.port)
+        connection = accept(server)
+    catch e
+        if isa(e, Base.IOError)
+            println("Failed to connect to $(tcp.protocol.ip):$(tcp.protocol.port) for IOError")
+        elseif isa(e, Base.UVError)
+            println("Failed to connect to $(tcp.protocol.ip):$(tcp.protocol.port) for UVError")
+        end
+        connection = nothing
+    end
+
+    if connection === nothing
+        sleep(5)
+        @goto pull_values_TCP
+    end
+
+    length = read(connection, sizeof(T))
+    length = reinterpret(UInt64, length)[1]
+    data = read(connection, length * sizeof(T))
+    values = reinterpret(T, data)
+
+    for value in values
+        put!(buffer, value)
+    end
+end
+function stream_push_values!(tcp::TCP, T, our_store::StreamStore, their_stream::Stream, buffer)
+    values = _load_val_from_buffer!(buffer, T)
+    @label push_values_TCP
+    try
+        connection = connect(tcp.protocol.ip, tcp.protocol.port)
+    catch e
+        if isa(e, Base.IOError)
+            println("Failed to connect to $(tcp.protocol.ip):$(tcp.protocol.port) for IOError")
+        elseif isa(e, Base.UVError)
+            println("Failed to connect to $(tcp.protocol.ip):$(tcp.protocol.port) for UVError")
+        end
+        connection = nothing
+    end
+    if connection === nothing
+        @goto push_values_TCP
+    end
+    write(connection, length(values))
+    write(connection, values)
+    close(connection)
+end
+
+#=
+# NATS dispatch
+function stream_pull_values!(nats::NATS, T, our_store::StreamStore, their_stream::Stream, buffer)
+    @label pull_values_NATS
+    try
+        nc = NATS.connect("nats://$(nats.protocol.ip):$(nats.protocol.port)")
+    catch e
+        println("Failed connecting to NATS at $(nats.protocol.ip):$(nats.protocol.port).")
+        nc = nothing
+    end
+
+    if nc === nothing
+        sleep(5)
+        @goto pull_values_NATS
+    end
+
+    function msg_handler(msg, T, buffer::Blocal)
+        data = Vector{UInt8}(msg.payload)
+        iob = IOBuffer(data)
+        length = read(buf, Int)
+
+        value_data = read(iob, sizeof(T) * length)
+        values = reinterpret(T, value_data)
+
+        for value in values
+            put!(buffer, value)
+        end
+    end
+
+    sub = subscribe(msg_handler, nc, nats.topic)
+end
+function stream_push_values!(nats::NATS, T, our_store::StreamStore, their_stream::Stream, buffer)
+    values = _load_val_from_buffer!(buffer, T)
+    @label push_values_NATS
+    try
+        nc = NATS.connect("nats://$(nats.protocol.ip):$(nats.protocol.port)")
+    catch e
+        println("Failed connecting to NATS at $(nats.protocol.ip):$(nats.protocol.port).")
+        nc = nothing
+    end
+
+    if nc === nothing
+        sleep(5)
+        @goto push_values_NATS
+    end
+
+    iob = IOBuffer()
+    write(iob, length(values))
+    write(iob, values)
+    data = String(take!(iob))
+    publish(nc, nats.topic, data)
+end
+
+# MQTT dispatch
+function stream_pull_values!(mqtt::MQTT, T, our_store::StreamStore, their_stream::Stream, buffer)
+    @label pull_values_MQTT
+    try
+        client = Mosquitto.Client(mqtt.protocol.ip, mqtt.protocol.port)
+    catch e
+        println("Failed connecting to MQTT Broker at $(mqtt.protocol.ip):$(mqtt.protocol.port).")
+        client = nothing
+    end
+    if client === nothing
+        sleep(5)
+        @goto pull_values_MQTT
+    end
+    subscribe(client, mqtt.topic)
+
+    Mosquitto.loop(client; timeout=500, ntimes=10)
+    msg_channel = get_messages_channel(client)
+
+    while !isempty(msg_channel)
+        msg = take!(msg_channel)
+        data = reinterpret(T, msg.payload)
+        for value in data
+            put!(buffer,data)
+        end
+    end
+end
+function stream_push_values!(mqtt::MQTT, T, our_store::StreamStore, their_stream::Stream, buffer)
+    values = _load_val_from_buffer!(buffer, T)
+    @label push_values_MQTT
+    try
+        client = Mosquitto.Client(mqtt.protocol.ip, mqtt.protocol.port)
+    catch e
+        println("Failed connecting to MQTT Broker at $(mqtt.protocol.ip):$(mqtt.protocol.port).")
+        client = nothing
+    end
+    if client === nothing
+        sleep(5)
+        @goto push_values_MQTT
+    end
+    data = reinterpret(UInt8, values)
+    publish(client, mqtt.topic, data; retain=true)
+end
+
+# FIXME:
+# Add ZeroMQ dispatch
+# ZeroMQ dispatch
+function stream_pull_values!(zeromq::ZeroMQ, T, our_store::StreamStore, their_stream::Stream, buffer)
+    values = _load_val_from_buffer!(buffer, T)
+    socket = Socket(Context(), REP)
+    @label push_values_MQTT
+    try
+        connect(socket, "tcp::$(zeromq.protocol.ip):$(zeromq.protocol.port)")
+    catch e
+        println("Failed connecting via ZeroMQ to $(zeromq.protocol.ip):$(zeromq.protocol.port).")
+        socket = nothing
+    end
+    if socket === nothing
+        sleep(5)
+        @goto push_values_MQTT
+    end
+    send(socket, values)
+    close(socket)
+end
+function stream_push_values!(zeromq::ZeroMQ, T, our_store::StreamStore, their_stream::Stream, buffer)
+    socket = Socket(Context(), REQ)
+    @label pull_values_MQTT
+    try
+        bind(socket, "tcp::$(zeromq.protocol.ip):$(zeromq.protocol.port)")
+    catch e
+        println("Failed connecting via ZeroMQ to $(zeromq.protocol.ip):$(zeromq.protocol.port).")
+        socket = nothing
+    end
+    if socket === nothing
+        sleep(5)
+        @goto pull_values_MQTT
+    end
+    values = recv(socket, Vector{T})
+
+    for value in values
+        put!(buffer, values)
+    end
+end
+=#
+
 #= TODO: Remove me
 # This is a bad implementation because it wants to sleep on the remote side to
 # wait for values, but this isn't semantically valid when done with MemPool.access_ref
