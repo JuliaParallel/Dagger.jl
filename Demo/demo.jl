625b4d7fd8ff7a309d51c3e24793088f4e50c648
Julian P Samaroo <jpsamaroo@gmail.com>
2025-04-11 15:24:31 -0700

    DEMO updates

--- Demo/demo.jl
+++ Demo/demo.jl
@@ -7,10 +7,19 @@ using Profile
 using Plots, DataFrames
 using GraphViz
 
+function enable_dagdebug()
+    ENV["JULIA_DEBUG"] = "Dagger"
+    empty!(Dagger.DAGDEBUG_CATEGORIES)
+    push!(Dagger.DAGDEBUG_CATEGORIES, :spawn_datadeps)
+end
+function disable_dagdebug()
+    ENV["JULIA_DEBUG"] = ""
+end
 function demo()
     Dagger.MemPool.MEM_RESERVED[] = 0
 
     N = 3000
+    valid = true
 
     # Benchmark
     println("Benchmarking FFTW")
@@ -20,50 +29,73 @@ function demo()
     FFTW.set_num_threads(Threads.nthreads())
     @assert FFTW.get_num_threads() == Threads.nthreads()
     @time fft!(input_fftw)
+    @assert input_fftw ≈ fft(input_dagger)
 
-    ENV["JULIA_DEBUG"] = ""
-    push!(Dagger.DAGDEBUG_CATEGORIES, :spawn_datadeps)
     FFTW.set_num_threads(1)
 
+    #=
     println("Benchmarking Dagger")
     GC.enable(false)
     @time Dagger.fft!(input_dagger)
     GC.enable(true)
     valid = input_fftw ≈ input_dagger
+    @show norm(input_fftw - input_dagger)
+    =#
 
+    #==#
     println("Benchmarking Dagger (inner)")
+    input_dagger_inner_copy = copy(input_dagger_inner)
     A, B = Dagger._fft_prealloc(Dagger.Pencil(), input_dagger_inner)
-    copyto!(A, input_dagger_inner)
     wait.(A.chunks)
     wait.(B.chunks)
     GC.enable(false)
+    copyto!(A, input_dagger_inner)
     @time Dagger._fft!(Dagger.Pencil(), A, B; dims=(1, 2))
+    copyto!(input_dagger_inner, B)
     GC.enable(true)
+    valid = input_fftw ≈ input_dagger_inner
+    #==#
 
-    #= Profiling
+    #=
     println("Profiling")
     input_dagger_profiling = rand(ComplexF64, N, N)
     GC.enable(false)
-    @profview @time Dagger.fft!(input_dagger_profiling)
+    Profile.@profile @time Dagger.fft!(input_dagger_profiling)
     GC.enable(true)
+    VSCodeServer.view_profile()
     =#
 
-    # Profiling (inner)
+    #==#
     println("Profiling (inner)")
     input_dagger_profiling_inner = rand(ComplexF64, N, N)
     A, B = Dagger._fft_prealloc(Dagger.Pencil(), input_dagger_profiling_inner)
-    copyto!(A, input_dagger_profiling_inner)
     wait.(A.chunks)
     wait.(B.chunks)
     GC.enable(false)
-    @profview @time Dagger._fft!(Dagger.Pencil(), A, B; dims=(1, 2))
+    copyto!(A, input_dagger_profiling_inner)
+    Profile.@profile @time Dagger._fft!(Dagger.Pencil(), A, B; dims=(1, 2))
+    copyto!(input_dagger_profiling_inner, B)
     GC.enable(true)
+    VSCodeServer.view_profile()
+    #==#
 
-    # Plotting
-    println("Plotting (inner)")
+    #=
+    println("Plotting")
     input_dagger_plotting = rand(ComplexF64, N, N)
-    A, B = Dagger._fft_prealloc(Dagger.Pencil(), input_dagger_plotting)
-    copyto!(A, input_dagger_plotting)
+    Dagger.enable_logging!(; metrics=false, all_task_deps=true)
+    GC.enable(false)
+    @time Dagger.fft!(input_dagger_plotting)
+    GC.enable(true)
+    logs = Dagger.fetch_logs!()
+    Dagger.disable_logging!()
+    display(Dagger.render_logs(logs, :plots_gantt; target=:execution, color_init_hash=UInt(1)))
+    =#
+
+    #==#
+    println("Plotting (inner)")
+    input_dagger_plotting_inner = rand(ComplexF64, N, N)
+    A, B = Dagger._fft_prealloc(Dagger.Pencil(), input_dagger_plotting_inner)
+    copyto!(A, input_dagger_plotting_inner)
     wait.(A.chunks)
     wait.(B.chunks)
     Dagger.enable_logging!(; metrics=false, all_task_deps=true)
@@ -72,7 +104,9 @@ function demo()
     GC.enable(true)
     logs = Dagger.fetch_logs!()
     Dagger.disable_logging!()
-    display(Dagger.render_logs(logs, :plots_gantt; target=:execution))
+    display(Dagger.render_logs(logs, :plots_gantt; target=:execution, color_init_hash=UInt(1)))
+    display(Dagger.render_logs(logs, :plots_gantt; target=:scheduler, color_init_hash=UInt(1)))
+    #==#
 
     return valid
 end
