09b38825750d9a89d0eb5c4be7d4d6cf5609c282
Akhil Akkapelli <akhilakkapelli@gmail.com>
2025-07-11 11:34:25 +0530

    Fix logical condition for singular value exception check in LU tests

--- test/array/linalg/lu.jl
+++ test/array/linalg/lu.jl
@@ -36,5 +36,5 @@
     @test lu!(rand(Blocks(64, 32), T, 128, 128), pivot) isa LU{T,DMatrix{T},DVector{Int}}
 
     # Singular Values
-    pivot == RowMaximum() || @test_throws LinearAlgebra.SingularException lu(ones(Blocks(64,64), T, 128, 128)) # FIXME: NoPivot needs Singular Exception Check
+    @test_throws LinearAlgebra.SingularException lu(ones(Blocks(64,64), T, 128, 128)) # FIXME: NoPivot needs Singular Exception Check
 end
