759a79c0d73bff14f7863778b57cd01ccccfe104
Akhil Akkapelli <akhilakkapelli@gmail.com>
2025-08-11 03:11:48 +0530

    Add linear algebra functions for matrix inversion 'inv' and triangular solve `ldiv!`

--- src/array/linalg.jl
+++ src/array/linalg.jl
@@ -66,3 +66,116 @@ function LinearAlgebra.ishermitian(A::DArray{T,2}) where T
 
     return all(fetch, to_check)
 end
+
+DMatrix{T}(::LinearAlgebra.UniformScaling, m::Int, n::Int, IBlocks::Blocks) where T = DMatrix(Matrix{T}(I, m, n), IBlocks)
+
+DMatrix{T}(::LinearAlgebra.UniformScaling, size::Tuple, IBlocks::Blocks) where T = DMatrix(Matrix{T}(I, size), IBlocks)
+
+function LinearAlgebra.inv(F::LU{T,<:DMatrix}) where T 
+    n = size(F, 1)
+    dest = DMatrix{T}(I, n, n, F.factors.partitioning)
+    LinearAlgebra.ldiv!(F, dest)
+    return dest
+end
+
+function LinearAlgebra.inv(A::LowerTriangular{T,<:DMatrix}) where T
+    S = typeof(LinearAlgebra.inv(oneunit(T)))
+    dest = DMatrix{S}(I, size(A), A.data.partitioning)
+    LinearAlgebra.ldiv!(convert(AbstractArray{S}, A), dest)
+    dest = LowerTriangular(dest)
+    return dest
+end
+
+function LinearAlgebra.inv(A::UpperTriangular{T,<:DMatrix}) where T
+    S = typeof(LinearAlgebra.inv(oneunit(T)))
+    dest = DMatrix{S}(I, size(A), A.data.partitioning)
+    LinearAlgebra.ldiv!(convert(AbstractArray{S}, A), dest)
+    dest = UpperTriangular(dest)
+    return dest
+end
+
+function LinearAlgebra.inv(A::UnitLowerTriangular{T,<:DMatrix}) where T
+    S = typeof(LinearAlgebra.inv(oneunit(T)))
+    dest = DMatrix{S}(I, size(A), A.data.partitioning)
+    LinearAlgebra.ldiv!(convert(AbstractArray{S}, A), dest)
+    dest = UnitLowerTriangular(dest)
+    return dest
+end
+
+function LinearAlgebra.inv(A::UnitUpperTriangular{T,<:DMatrix}) where T
+    S = typeof(LinearAlgebra.inv(oneunit(T)))
+    dest = DMatrix{S}(I, size(A), A.data.partitioning)
+    LinearAlgebra.ldiv!(convert(AbstractArray{S}, A), dest)
+    dest = UnitUpperTriangular(dest)
+    return dest
+end
+
+function LinearAlgebra.inv(A::DMatrix{T}) where T
+    n = LinearAlgebra.checksquare(A)
+    S = typeof(zero(T)/one(T))      # dimensionful
+    S0 = typeof(zero(T)/oneunit(T)) # dimensionless
+    dest = DMatrix{S0}(I, n, n, A.partitioning)
+    F = factorize(convert(AbstractMatrix{S}, A))
+    LinearAlgebra.ldiv!(F, dest)
+    return dest
+end
+
+
+function LinearAlgebra.ldiv!(A::LU{<:Any,<:DMatrix}, B::AbstractVecOrMat)
+    allowscalar(true) do
+        LinearAlgebra._apply_ipiv_rows!(A, B)
+    end
+    LinearAlgebra.ldiv!(UnitLowerTriangular(A.factors), B)
+    LinearAlgebra.ldiv!(UpperTriangular(A.factors), B)
+end
+
+function LinearAlgebra.ldiv!(A::Union{LowerTriangular{<:Any,<:DMatrix},UnitLowerTriangular{<:Any,<:DMatrix},UpperTriangular{<:Any,<:DMatrix},UnitUpperTriangular{<:Any,<:DMatrix}}, B::AbstractVecOrMat)
+    alpha = one(eltype(A))
+    trans = 'N'
+    diag = isa(A, UnitUpperTriangular) || isa(A, UnitLowerTriangular) ? 'U' : 'N'
+
+    if isa(A, UpperTriangular) || isa(A, UnitUpperTriangular)
+        uplo = 'U'
+    elseif isa(A, LowerTriangular) || isa(A, UnitLowerTriangular)
+        uplo = 'L'
+    end
+
+    dB = B isa DVecOrMat ? B : (B isa AbstractMatrix ? view(B, A.data.partitioning) : view(B, AutoBlocks()))
+
+    parent_A = parent(A)
+    if isa(B, AbstractVector)
+        min_bsa = min(min(parent_A.partitioning.blocksize...), dB.partitioning.blocksize[1])
+        Dagger.maybe_copy_buffered(parent_A => Blocks(min_bsa, min_bsa), dB=>Blocks(min_bsa)) do parent_A, dB
+            Dagger.trsv!(uplo, trans, diag, alpha, parent_A, dB)
+        end
+    elseif isa(B, AbstractMatrix)
+        min_bsa = min(parent_A.partitioning.blocksize...)
+        Dagger.maybe_copy_buffered(parent_A => Blocks(min_bsa, min_bsa), dB=>Blocks(min_bsa, min_bsa)) do parent_A, dB
+            Dagger.trsm!('L', uplo, trans, diag, alpha, parent_A, dB)
+        end
+    end
+end
+
+function LinearAlgebra.ldiv!(Y::DArray, A::DMatrix, B::DArray)
+    LinearAlgebra.ldiv!(A, copyto!(Y, B))
+end
+
+function LinearAlgebra.ldiv!(A::DMatrix, B::DArray)
+    LinearAlgebra.ldiv!(LinearAlgebra.lu(A), B)
+end
+
+function LinearAlgebra.ldiv!(C::DVecOrMat, A::Union{LowerTriangular{<:Any,<:DMatrix},UnitLowerTriangular{<:Any,<:DMatrix},UpperTriangular{<:Any,<:DMatrix},UnitUpperTriangular{<:Any,<:DMatrix}}, B::DVecOrMat)
+    LinearAlgebra.ldiv!(A, copyto!(C, B))
+end
+
+function LinearAlgebra.ldiv!(C::Cholesky{<:Any,<:DMatrix}, B::DVecOrMat)
+    # L * y = B
+    y = copyto!(similar(B), B)
+    LinearAlgebra.ldiv!(C.L, y)
+
+    # L' * x = y
+    copyto!(B, y)
+    LinearAlgebra.ldiv!(C.U, B)
+
+    return B
+end
