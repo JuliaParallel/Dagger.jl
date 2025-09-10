e9210e88f202574d2eee63e5e585328efc2fa61e
Julian P Samaroo <jpsamaroo@gmail.com>
2025-09-10 17:30:21 +0000

    datadeps: Use LFU cache for scope lookup

--- src/options.jl
+++ src/options.jl
@@ -155,30 +155,6 @@ function maybe_default!(opts::Options, ::Val{opt}, sig::Signature) where opt
     end
 end
 
-struct BasicLFUCache{K,V}
-    cache::Dict{K,V}
-    freq::Dict{K,Int}
-    max_size::Int
-
-    BasicLFUCache{K,V}(max_size::Int) where {K,V} = new(Dict{K,V}(), Dict{K,Int}(), max_size)
-end
-function Base.get!(f, cache::BasicLFUCache{K,V}, key::K) where {K,V}
-    if haskey(cache.cache, key)
-        cache.freq[key] += 1
-        return cache.cache[key]
-    end
-    val = f()::V
-    cache.cache[key] = val
-    cache.freq[key] = 1
-    if length(cache.cache) > cache.max_size
-        # Find the least frequently used key
-        _, lfu_key::K = findmin(cache.freq)
-        delete!(cache.cache, lfu_key)
-        delete!(cache.freq, lfu_key)
-    end
-    return val
-end
-
 const SIGNATURE_DEFAULT_CACHE = TaskLocalValue{BasicLFUCache{Tuple{UInt,Symbol},Any}}(()->BasicLFUCache{Tuple{UInt,Symbol},Any}(256))
 
 # SchedulerOptions integration
