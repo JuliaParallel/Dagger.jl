50c41e7bf141eebafe4ee296178bdbae183a9527
Felipe Tome <tomefelipe0@usp.br>
2025-09-19 12:53:51 -0300

    CAQR: final working copy and adjusted NextLA calls

--- src/Dagger.jl
+++ src/Dagger.jl
@@ -8,7 +8,7 @@ import MemPool
 import MemPool: DRef, FileRef, poolget, poolset
 
 import Base: collect, reduce
-
+import NextLA
 import LinearAlgebra
 import LinearAlgebra: Adjoint, BLAS, Diagonal, Bidiagonal, Tridiagonal, LAPACK, LowerTriangular, PosDefException, Transpose, UpperTriangular, UnitLowerTriangular, UnitUpperTriangular, diagind, ishermitian, issymmetric
 import Random
@@ -109,6 +109,7 @@ include("array/linalg.jl")
 include("array/mul.jl")
 include("array/cholesky.jl")
 include("array/lu.jl")
+include("array/qr.jl")
 
 import KernelAbstractions, Adapt
 
