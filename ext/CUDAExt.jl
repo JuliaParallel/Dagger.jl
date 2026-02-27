a5546dff783aebbdd694e04ee145fb99a1ce0293
Julian P Samaroo <jpsamaroo@jpsamaroo.me>
2026-02-26 19:41:51 -0500

    LU: Fixes for GPU execution

--- ext/CUDAExt.jl
+++ ext/CUDAExt.jl
@@ -292,11 +292,14 @@ end
 
 # Adapt BLAS/LAPACK functions
 import LinearAlgebra: BLAS, LAPACK
+_keep_blas_functions = Set(["iamax"])
 for lib in [BLAS, LAPACK]
     for name in names(lib; all=true)
         name == nameof(lib) && continue
         startswith(string(name), '#') && continue
-        endswith(string(name), '!') || continue
+        if !endswith(string(name), '!') && !any(endswith(string(name), func) for func in _keep_blas_functions)
+            continue
+        end
 
         for culib in [CUBLAS, CUSOLVER]
             if name in names(culib; all=true)
