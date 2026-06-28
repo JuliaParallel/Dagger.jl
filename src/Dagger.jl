4b6dddf281d4773fe37a045b8d0a245cdb85d98d
Julian P Samaroo <jpsamaroo@gmail.com>
2026-06-28 10:23:47 -0700

    Remove Requires dep

--- src/Dagger.jl
+++ src/Dagger.jl
@@ -23,10 +23,6 @@ else
 end
 import TaskLocalValues: TaskLocalValue
 
-if !isdefined(Base, :get_extension)
-    import Requires: @require
-end
-
 import TimespanLogging
 import TimespanLogging: timespan_start, timespan_finish
 
@@ -170,42 +166,7 @@ function __init__()
     delete!(SYSTEM_UUIDS, myid())
     system_uuid()
 
-    @static if !isdefined(Base, :get_extension)
-        @require Distributions="31c24e10-a181-5473-b8eb-7969acd0382f" begin
-            include(joinpath(dirname(@__DIR__), "ext", "DistributionsExt.jl"))
-        end
-        @require Graphs="86223c79-3864-5bf0-83f7-82e725a168b6" begin
-            @require GraphViz="f526b714-d49f-11e8-06ff-31ed36ee7ee0" begin
-                include(joinpath(dirname(@__DIR__), "ext", "GraphVizExt.jl"))
-            end
-        end
-        @require Colors="5ae59095-9a9b-59fe-a467-6f913c188581" begin
-            include(joinpath(dirname(@__DIR__), "ext", "GraphVizSimpleExt.jl"))
-            # TODO: Move to Pkg extensions
-            @require Luxor="ae8d54c2-7ccd-5906-9d76-62fc9837b5bc" begin
-                # Gantt chart renderer
-                include("ui/gantt-luxor.jl")
-            end
-            @require Mux="a975b10e-0019-58db-a62f-e48ff68538c9" begin
-                # Gantt chart HTTP server
-                include("ui/gantt-mux.jl")
-            end
-            @require JSON3 = "0f8b85d8-7281-11e9-16c2-39a750bddbf1" begin
-                include(joinpath(dirname(@__DIR__), "ext", "JSON3Ext.jl"))
-            end
-        end
-        # TODO: Move to Pkg extensions
-        @require ProfileSVG="132c30aa-f267-4189-9183-c8a63c7e05e6" begin
-            # Profile renderer
-            include("ui/profile-profilesvg.jl")
-        end
-        @require FFMPEG="c87230d0-a227-11e9-1b43-d7ebe4e7570a" begin
-            @require FileIO="5789e2e9-d7fb-5bc7-8068-2c6fae9b9549" begin
-                # Video generator
-                include("ui/video.jl")
-            end
-        end
-    end
+
     for tid in 1:Threads.nthreads()
         add_processor_callback!("__cpu_thread_$(tid)__") do
             ThreadProc(myid(), tid)
